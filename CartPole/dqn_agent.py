a21bbb92f25742ecdbcccc2232a13d25b2e5489b
kamito300 <tkammyz@gmail.com>
2016-12-08 20:09:37 +0900

    update

--- CartPole/dqn_agent.py
+++ CartPole/dqn_agent.py
@@ -12,10 +12,11 @@ class Model(Chain):
   def __init__(self):
     super(Model, self).__init__(
       l1 = L.Linear(4, 16),
-      l2 = L.Linear(16, 256),
-      l3 = L.Linear(256, 512),
-      l4 = L.Linear(512, 1024),
-      l5 = L.Linear(1024, 2),
+      l2 = L.Linear(16, 32),
+      l3 = L.Linear(32, 64),
+      l4 = L.Linear(64, 32),
+      l5 = L.Linear(32, 16),
+      l6 = L.Linear(16, 2),
     )
 
   def __call__(self, x, y):
@@ -26,8 +27,10 @@ class Model(Chain):
     h2 = F.leaky_relu(self.l2(h1))
     h3 = F.leaky_relu(self.l3(h2))
     h4 = F.leaky_relu(self.l4(h3))
-    y = F.leaky_relu(self.l5(h4))
+    h5 = F.leaky_relu(self.l5(h4))
+    y = F.leaky_relu(self.l6(h5))
     return y
+
 
 class Agent:
   def __init__(self):
@@ -36,10 +39,12 @@ class Agent:
     self.optimizer.setup(self.model)
     self.experience = []
     self.max_experience = 300 * 100
-    self.epsilon = 0.5
-    self.decay = 0.99
-    self.batch_size = 32
+    self.epsilon = 0.99
+    self.decay = 0.999
+    self.batch_size = 256 
     self.gamma = 0.9
+    self.loss = None 
+  
 
   def predict_action(self, state):
     x = Variable(np.array(state, dtype=np.float32).reshape((1, -1)))
@@ -50,14 +55,16 @@ class Agent:
     if np.random.random() < self.epsilon:
       action = random.randint(0,1)
       self.epsilon = self.decay * self.epsilon
+      # print("random action: %f, epsilon: %f" % (action, self.epsilon))
     else:
       action = np.argmax(self.predict_action(state))
+      # print("greedy action: %f" % action)
     return action
 
-  def save_experience(self, old_state, action, reward, new_state):
-    self.experience.append({"old_state": old_state, "action": action, "reward": reward, "new_state": new_state})
-    self.experience.sort(key=lambda x:x["reward"])
-    if len(self.experience) > self.max_experience:
+  def save_experience(self, exp):
+    self.experience.append(exp)
+    # self.experience.sort(key=lambda x:x["reward"])
+    while len(self.experience) > self.max_experience:
       self.experience.pop
 
   def replay(self):
@@ -65,14 +72,20 @@ class Agent:
       return
           
     batch = np.array(random.sample(self.experience, self.batch_size))
+    # print(batch)
     x = Variable(np.array(map(lambda x:x["old_state"], batch), dtype=np.float32))
     labels = np.array(self.model.predict(x).data.copy(), dtype=np.float32)
     for i in range(self.batch_size):
-      action, reward, new_state = batch[i]["action"], batch[i]["reward"], batch[i]["new_state"]
-      labels[i, action] = reward + self.gamma * np.max(self.predict_action(new_state))
+      action, reward, new_state, done = batch[i]["action"], batch[i]["reward"], batch[i]["new_state"], batch[i]["done"]
+      
+      if done:
+        labels[i, action] = reward
+      else:
+        labels[i, action] = reward + self.gamma * np.max(self.predict_action(new_state))
 
     self.model.zerograds()
     loss = self.model(x, Variable(labels)) 
+    self.loss = loss
     loss.backward()
     self.optimizer.update()
       
@@ -80,27 +93,40 @@ class Agent:
 class Trainer:
   def __init__(self):
     self.agt = Agent()
+    self.exp = []
+    self.logfile = "result.log"
+    self.episode = 0
 
   def train(self):
     env = gym.make('CartPole-v0')
+    # with open(self.logfile, 'w') as f:
+    #   f.write('episode, timestamp')
     for i in range(30000):
       print("episode: %d" % i)
+      print("    epsilon      : %f" % self.agt.epsilon)
       state = env.reset()
+      total_rewards = 0
+      self.exp = []
+      self.episode = i
 
       for t in range(300):
         env.render()
         old_state = state.copy()
         action = self.agt.action(state)
         state, reward, done, info = env.step(action)
-        if done:
-          reward = -200 
+        total_rewards += reward
         new_state = state.copy()
-        self.agt.save_experience(old_state, action, reward, new_state)
+        self.agt.save_experience({"old_state": old_state, "action": action, "reward": reward, "new_state": new_state, "done": done})
         if done:
-          print("timestamp is %d" % t)
+          # with open(self.logfile, 'w') as f:
+          #   f.write(str(self.episode) + ',' + str(t) + '\n')
+          print("    timestamp    : %d" % t)
           break
 
       self.agt.replay()
+      if self.agt.loss is not None:
+        print("    loss         : %f" % self.agt.loss.data)
+        print("    total rewards: %f" % total_rewards)
 
 trainer = Trainer()
 trainer.train()
